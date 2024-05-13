ca435e4bd1b2cfe7ab26c614b2a99948ea01f826
William Mallett <mallett002@gmail.com>
2024-05-13 06:23:14 -0500

    updating pointers

--- tour-of-go/3-more-types/main.go
+++ tour-of-go/3-more-types/main.go
@@ -17,9 +17,11 @@ func pointerBasics() {
 
 	p := &i 							// point to i
 	fmt.Printf("\n*p is: %v", *p)		// read i through the pointer
-	fmt.Printf("\np is: ", p)			// print memory address of i
+	fmt.Printf("\np is %v: ", p)		// print memory address of i
 	*p = 21								// set i through the pointer
 	fmt.Printf("\ni is: %v\n", i)
+
+	fmt.Println("")
 
 	fmt.Printf("j is: %v\n", j)
 	p = &j								// point to j
