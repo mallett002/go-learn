9c9e5f3cfb9c22d501dd762846a743cdc0264617
William Mallett <mallett002@gmail.com>
2024-05-13 06:20:05 -0500

    some pointers

---
+++ tour-of-go/3-more-types/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	pointers()
+}
+
+func pointers() {
+	pointerBasics()
+}
+
+func pointerBasics() {
+	i, j := 42, 2701
+
+	p := &i 							// point to i
+	fmt.Printf("\n*p is: %v", *p)		// read i through the pointer
+	fmt.Printf("\np is: ", p)			// print memory address of i
+	*p = 21								// set i through the pointer
+	fmt.Printf("\ni is: %v\n", i)
+
+	fmt.Printf("j is: %v\n", j)
+	p = &j								// point to j
+	*p = *p / 37						// divide j through the pointer
+	fmt.Printf("j is now: %v", j)
+}
